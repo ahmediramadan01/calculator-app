a6f8864f295b2a42cf97f72f36daff67251773db
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-12-14 14:14:57 +0200

    Build theme toggle button and Store prefered theme in local storage

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 "use script";
+
+const btnToggle = document.querySelector(".btn--toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (!currentTheme) localStorage.setItem("theme", "dark");
+else if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btnToggle.addEventListener("click", function () {
+  let theme;
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
