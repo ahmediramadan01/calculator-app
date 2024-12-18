6f414d2373275e677e785c7947e613a9106f8d05
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-12-18 22:13:08 +0200

    Implement operators functionality

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const btnDecimal = document.querySelector(".btn--decimal");
 const btnPercentage = document.querySelector(".btn--percentage");
 const btnClear = document.querySelector(".btn--clear");
 const btnAllClear = document.querySelector(".btn--all-clear");
+const btnsOperator = document.querySelectorAll(".btn--operator");
 
 /* Theme toggle */
 
@@ -61,7 +62,7 @@ function multiply(x, y) {
 
 function divide(x, y) {
   if (parseFloat(y) === 0) return undefined;
-  return parseFloat(x) / parseFloat(x);
+  else return parseFloat(x) / parseFloat(y);
 }
 
 function operate(firstOperand, secondOperand, mathOperator) {
@@ -85,6 +86,36 @@ function updateScreen() {
   operatorElement.innerHTML = screenOperator;
 }
 updateScreen();
+
+/* Operators markup */
+
+const addMarkup = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M10.929 19.5v-6.429H4.5V10.93h6.429V4.5h2.142v6.429H19.5v2.142h-6.429V19.5H10.93z"
+      fill="#fff" />
+  </svg>
+`;
+
+const subtractMarkup = `
+  <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7.5 21.6666V18.3333H32.5V21.6666H7.5Z" fill="#fff" />
+  </svg>
+`;
+
+const multiplyMarkup = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4.5 6 6 4.5l6 6 6-6L19.5 6l-6 6 6 6-1.5 1.5-6-6-6 6L4.5 18l6-6-6-6z" fill="#fff" />
+  </svg>
+`;
+
+const divideMarkup = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4.5 10.929h15v2.142h-15V10.93zM12 19.5a2.143 2.143 0 1 1 0-4.286 2.143 2.143 0 0 1 0 4.286zm0-10.714A2.143 2.143 0 1 1 12 4.5a2.143 2.143 0 0 1 0 4.286z"
+      fill="#fff" />
+  </svg>
+`;
 
 /* Callback functions */
 
@@ -145,6 +176,40 @@ function clearScreen() {
   updateScreen();
 }
 
+function appendOperator(event) {
+  if (!operand && !operator) {
+    operand = result;
+    screenOperand = operand;
+
+    result = "0";
+    screenResult = result;
+  } else if (operand && operator) {
+    operand = operate(operand, result, operator);
+    screenOperand = operand;
+
+    result = "0";
+    screenResult = result;
+  }
+
+  operator = event.target?.closest(".btn--operator").dataset.value || event;
+  switch (operator) {
+    case "+":
+      screenOperator = addMarkup;
+      break;
+    case "-":
+      screenOperator = subtractMarkup;
+      break;
+    case "*":
+      screenOperator = multiplyMarkup;
+      break;
+    case "/":
+      screenOperator = divideMarkup;
+      break;
+  }
+
+  updateScreen();
+}
+
 /* Event handlers */
 
 btnsNum.forEach((btn) => {
@@ -159,6 +224,10 @@ btnClear.addEventListener("click", clear);
 
 btnAllClear.addEventListener("click", clearScreen);
 
+btnsOperator.forEach((btn) => {
+  btn.addEventListener("click", appendOperator);
+});
+
 window.addEventListener("keydown", function (event) {
   event.preventDefault();
 
@@ -172,5 +241,10 @@ window.addEventListener("keydown", function (event) {
     clear();
   } else if (event.key === "Escape") {
     clearScreen();
+  } else if (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
+    if (event.key === "+") appendOperator("+");
+    else if (event.key === "-") appendOperator("-");
+    else if (event.key === "*") appendOperator("*");
+    else if (event.key === "/") appendOperator("/");
   }
 });
