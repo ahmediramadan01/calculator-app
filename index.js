8ccdad7b5289a0f5885310e3b8472f04d8f4bdaa
Ahmed Ramadan <143494954+ahmediramadan01@users.noreply.github.com>
2024-12-18 15:24:22 +0200

    Implement clicking or pressing on a number functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const btnToggle = document.querySelector(".btn--toggle");
 const resultElement = document.querySelector(".result");
 const operandElement = document.querySelector(".operand");
 const operatorElement = document.querySelector(".operator");
+const btnsNum = document.querySelectorAll(".btn--num");
 
 /* Theme toggle */
 
@@ -80,3 +81,40 @@ function updateScreen() {
   operatorElement.innerHTML = screenOperator;
 }
 updateScreen();
+
+/* Callback functions */
+
+function appendNumber(event) {
+  const clickedNumber = event.key || event.target.dataset.value;
+  console.log(`🚀 ~ clickedNumber ->`, clickedNumber);
+
+  if (screenOperator === "" && screenResult.length <= 15) {
+    screenResult === "" || screenResult === "0"
+      ? (result = clickedNumber)
+      : (result += clickedNumber);
+
+    screenResult = result;
+  } else if (screenOperator === "" && screenOperand.length <= 15) {
+    screenOperand === "" || screenOperand === "0"
+      ? (operand = clickedNumber)
+      : (operand += clickedNumber);
+
+    screenOperand = operand;
+  }
+
+  updateScreen();
+}
+
+/* Event handlers */
+
+btnsNum.forEach((btn) => {
+  btn.addEventListener("click", appendNumber);
+});
+
+window.addEventListener("keydown", function (event) {
+  event.preventDefault();
+
+  if (!isNaN(+event.key)) {
+    appendNumber(event);
+  }
+});
